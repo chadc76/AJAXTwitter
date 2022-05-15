a481468c87b1c546154262ff39d6307af0a98c1b
Chad Cunningham <chadcunningham7676@gmail.com>
2022-05-14 20:10:11 -0400

    Added click handler

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -61,11 +61,33 @@
 	    this.followState = this.$el.data('initial-follow-state');
 	
 	    this.render();
+	    
+	    this.$el.on('click', this.handleClick.bind(this));
+	  }
+	
+	  handleClick(e) {
+	    e.preventDefault();
+	    let requestType = this.followState === 'followed' ? "DELETE" : "POST";
+	
+	    $.ajax({
+	      type: requestType,
+	      url: `${this.userId}/follow`,
+	      dataType: "json",
+	      data: this.$el.serialize(),
+	      success: function(r) {
+	        this.toggleFollow();
+	        this.render();
+	      }.bind(this)
+	    })
 	  }
 	
 	  render() {
-	    let text = this.followState === 'followed' ? 'Followed!' : 'Unfollowed!';
+	    let text = this.followState === 'followed' ? 'Unfollow!' : 'Follow!';
 	    this.$el.html(text);
+	  }
+	
+	  toggleFollow() {
+	    this.followState = this.followState === 'followed' ? 'unfollowed' : 'followed';
 	  }
 	};
 	
